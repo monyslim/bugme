bb47b6ac53d793fdd7c66a4052b33446bf3b004d
david <prcenturionboy@gmail.com>
2023-07-21 04:23:52 +0100

    bugme

---
+++ database/db.php
@@ -0,0 +1,9 @@
+<?php
+$__host = "mysql:3306";
+$__user = "root";
+$__password = "password";
+$__dbname = "bugme";
+
+
+$__conn = mysqli_connect($__host, $__user, $__password, $__dbname) or die("Could not connect to database ...");
+
